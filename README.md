e5c57e75053b1e96adfb61c7b7ec65820996d255
Robin Schellius <r.schellius@avans.nl>
2024-04-19 11:14:15 +0200

    Database added

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # My First Nodejs Express server
+
 Hier komt een korte introductie.
 
 ## Installing
+
 To install, run `npm install`.
 
 ## Running
-To run, type `npnm start`.
+
+To run, type `npm start`.
--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const dtb = require('./src/dtb/inmem-db')
 const app = express()
 
+app.all('*', (req, res, next) => {
+    console.log('Request:', req.method, req.url)
+    next()
+})
+
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.json({ message: 'Hello World' })
+})
+
+app.get('/api/info', (req, res) => {
+    console.log('GET /api/info')
+    const info = {
+        name: 'My Nodejs Express server',
+        version: '0.0.1',
+        description: 'This is a simple Nodejs Express server'
+    }
+    res.json(info)
+})
+
+app.get('/api/users', (req, res) => {
+    // console.log('GET /api/users')
+    dtb.getAll((err, data) => {
+        if (err) {
+            res.status(500).json(err)
+        } else {
+            res.status(200).json(data)
+        }
+    })
 })
 
 app.listen(3000)
