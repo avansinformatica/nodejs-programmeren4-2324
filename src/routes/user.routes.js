baa81ec2709cea1ee7b8d9bcadee278dd682d43b
casstaal <casstaal@hotmail.com>
2024-04-25 10:04:42 +0200

    Changed validateUser from user.controller to user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,28 @@ const router = express.Router()
 const userController = require('../controllers/user.controller')
 const assert = require('assert')
 
+const validateUser = (req, res, next) => {
+    let user = req.body
+    let { firstName, lastName, emailAdress } = user
+    try {
+        assert(typeof firstName === 'string', 'First name must be a string')
+        assert(typeof lastName === 'string', 'Last name must be a string')
+        assert(
+            typeof emailAdress === 'string',
+            'Email address must be a string'
+        )
+        next()
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({
+            status: 400,
+            message: err.message
+        })
+    }
+}
+
 // Userroutes
-router.post('/api/users', userController.validateUser, userController.create)
+router.post('/api/users', validateUser, userController.create)
 router.get('/api/users', userController.getAll)
 router.get('/api/users/:userId', userController.getById)
 router.delete('/api/users/:userId', userController.deleteUser)
