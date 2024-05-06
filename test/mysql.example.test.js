c5c24fb905b8fbda26bb534a1dee59fff7be8254
Robin Schellius <r.schellius@avans.nl>
2024-05-06 17:43:57 +0200

    MySql example in service toegevoegd

--- test/mysql.example.test.js
+++ test/mysql.example.test.js
@@ -1,5 +1,5 @@
 process.env.DB_DATABASE = process.env.DB_DATABASE || 'share-a-meal-testdb'
-process.env.LOGLEVEL = 'warn'
+process.env.LOGLEVEL = 'trace'
 
 const chai = require('chai')
 const chaiHttp = require('chai-http')
